1f0401c168aead62cf6d1b786f7f187d780de806
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-17 17:06:46 +0900

    Fix: 이미지 로직 수정 (#9)

--- src/main/java/com/gudgo/jeju/global/util/image/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gudgo.jeju.global.util.image.repository;
-
-import com.gudgo.jeju.global.util.image.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
