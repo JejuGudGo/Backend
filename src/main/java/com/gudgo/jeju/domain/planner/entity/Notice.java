c7573f803c7058e661755da8e4f72f7cd3f6607a
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-24 15:32:17 +0900

    Feat: Notice 테이블 추가 (#24)

--- src/main/java/com/gudgo/jeju/domain/planner/entity/Notice.java
+++ src/main/java/com/gudgo/jeju/domain/planner/entity/Notice.java
@@ -6,14 +6,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Chatting {
+public class Notice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String notice;
+
+
+    @ManyToOne
+    @JoinColumn(name = "chattingId")
+    private Chatting chatting;
+
+    @OneToOne
+    @JoinColumn(name = "participantId")
+    private Participant participant;
 }
