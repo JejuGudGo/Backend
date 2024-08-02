a0a0e117eb4f14665941991450006bdcf44070ed
codesooo <zlsoo9ee@gmail.com>
2024-08-02 10:42:23 +0900

    BREAKINGCHANGE: 유저 정보 관련 api 수정 (#40)

--- src/main/java/com/gudgo/jeju/domain/user/controller/UserInfoController.java
+++ src/main/java/com/gudgo/jeju/domain/user/controller/UserInfoController.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/api/v1/auth")
+@RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
