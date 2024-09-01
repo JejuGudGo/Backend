822ba03dfd647c1f57637d39185f075afea4e89f
codesooo <zlsoo9ee@gmail.com>
2024-09-01 23:21:27 +0900

    Breaking Change : 리뷰 기능 수정 - scheduler 생성 (#51)

--- src/main/java/com/gudgo/jeju/domain/planner/course/repository/CourseRepository.java
+++ src/main/java/com/gudgo/jeju/domain/planner/course/repository/CourseRepository.java
@@ -2,6 +2,11 @@ package com.gudgo.jeju.domain.planner.course.repository;
 
 import com.gudgo.jeju.domain.planner.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    @Query("SELECT c FROM Course c WHERE c.id = c.originalCourseId")
+    List<Course> findOriginalCourses();
 }
--- src/main/java/com/gudgo/jeju/domain/planner/planner/repository/PlannerRepository.java
+++ src/main/java/com/gudgo/jeju/domain/planner/planner/repository/PlannerRepository.java
@@ -3,6 +3,7 @@ package com.gudgo.jeju.domain.planner.planner.repository;
 import com.gudgo.jeju.domain.planner.planner.entity.Planner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlannerRepository extends JpaRepository<Planner, Long> {
@@ -10,4 +11,8 @@ public interface PlannerRepository extends JpaRepository<Planner, Long> {
     Optional<Planner> findByCourseId(Long courseId);
 
     Planner findByChatRoomId(Long chatRoomId);
+
+    List<Long> findPlannerIdsByCourseId(Long courseId);
+
+    List<Planner> findAllByCourseId(Long courseId);
 }
