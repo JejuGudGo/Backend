e0469c7fed244fb6b625ac5555b6872d5652d961
codesooo <zlsoo9ee@gmail.com>
2024-09-23 00:43:49 +0900

    Fix : TodoConstants 수정 (#47)

--- src/main/java/com/gudgo/jeju/domain/todo/entity/TodoConstants.java
+++ src/main/java/com/gudgo/jeju/domain/todo/entity/TodoConstants.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class TodoConstants {
     public static final List<String> DEFAULT_TODO_ITEMS = Arrays.asList(
-            "등산화", "모자", "우비", "배낭", "생수", "가벼운 구급약", "랜턴", "선글라스", "간식", "잠옷"
+            "트래킹화 혹은 등산화", "모자", "우산 혹은 우의", "편한 옷", "넉넉한 배낭", "생수 1병 혹은 텀블러", "가벼운 구급약", "랜턴", "선글라스", "간식"
     );
 }
