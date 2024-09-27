55ad352b4b71740dae0ba6db77e7a14e4d333648
sieunnnn <wldsmtldsm65@gmail.com>
2024-09-27 12:43:10 +0900

    Fix: 뱃지 부여 로직 수정 (#47)

--- src/main/java/com/gudgo/jeju/domain/badge/repository/BadgeRepository.java
+++ src/main/java/com/gudgo/jeju/domain/badge/repository/BadgeRepository.java
@@ -3,10 +3,17 @@ package com.gudgo.jeju.domain.badge.repository;
 import com.gudgo.jeju.domain.badge.entity.Badge;
 import com.gudgo.jeju.domain.badge.entity.BadgeCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
     List<Badge> findByUserId(Long userId);
-    boolean existsByUserIdAndCode(Long userId, BadgeCode badgeCode);
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END " +
+            "FROM Badge b WHERE b.user.id = :userId AND b.code = :badgeCode")
+    boolean existsByUserIdAndCode(@Param("userId") Long userId, @Param("badgeCode") BadgeCode badgeCode);
+
+
 }
