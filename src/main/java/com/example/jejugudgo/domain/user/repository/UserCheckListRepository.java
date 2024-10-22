9a17c4e248e695c27d65f9a0af2f5a932a89ee8f
jisoo <zlsoo9ee@gmail.com>
2024-10-22 22:38:35 +0900

    72 validate 체크리스트 기능 점검 (#77)
    * Feat : 체크리스트 엔티티 생성 (#72)
    
    * Feat : 체크리스트 기능 구현 - 생성,목록조회,상세조회 (#72)
    
    * Feat : 체크리스트 기능 구현 - 내용 수정, 완료 처리 (#72)
    
    * Refac : 체크리스트 코드 리팩토링 (#72)
    
    * Refac : update API 수정 (#72)
    
    * fix : update 로직 오류 수정 (#72)
    
    * fix : 체크리스트 항목 순서 변경 가능하도록 수정 (#72)
    
    * fix : repository 수정 반영 (#72)

--- src/main/java/com/example/jejugudgo/domain/user/repository/UserCheckListRepository.java
+++ src/main/java/com/example/jejugudgo/domain/user/repository/UserCheckListRepository.java
@@ -4,7 +4,10 @@ import com.example.jejugudgo.domain.user.entity.UserCheckList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserCheckListRepository extends JpaRepository<UserCheckList, Long> {
     List<UserCheckList> findAllByUserId(Long userId);
+
+    Optional<UserCheckList> findTopByUserIdOrderByOrderNumberDesc(Long userId);
 }
