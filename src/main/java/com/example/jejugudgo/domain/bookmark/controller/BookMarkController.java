32d3fb1d06d9c995b5235eec5123d4787d709db3
codesooo <zlsoo9ee@gmail.com>
2024-10-27 18:31:02 +0900

    Fix : 북마크 컨트롤러 수정 (#78)

--- src/main/java/com/example/jejugudgo/domain/bookmark/controller/BookMarkController.java
+++ src/main/java/com/example/jejugudgo/domain/bookmark/controller/BookMarkController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/bookmark")
+@RequestMapping(value = "/api/v1/users/bookmark")
 @RequiredArgsConstructor
 public class BookMarkController {
     private final BookMarkService bookMarkService;
