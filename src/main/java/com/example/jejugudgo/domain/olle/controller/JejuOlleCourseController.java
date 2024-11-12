28573f522bab70ff6767efdec400ca279575f066
codesooo <zlsoo9ee@gmail.com>
2024-11-12 14:51:08 +0900

    Fix : 올레 코스 반환 오류 수정 (#94)

--- src/main/java/com/example/jejugudgo/domain/olle/controller/JejuOlleCourseController.java
+++ src/main/java/com/example/jejugudgo/domain/olle/controller/JejuOlleCourseController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/api/v1/courses/olle")
 @RequiredArgsConstructor
@@ -25,7 +29,17 @@ public class JejuOlleCourseController {
     @GetMapping("")
     public ResponseEntity<CommonApiResponse> getJejuOlleCourses(Pageable pageable) {
         Page<JejuOlleCourseResponseForList> response = jejuOlleCourseQueryService.getOlleCourses(pageable);
-        return ResponseEntity.ok(apiResponseUtil.success(response));
+        Map<String, Object> responseData = new LinkedHashMap<>();
+        responseData.put("jejuOlleCourse", response.getContent());
+        responseData.put("pageable", response.getPageable());
+        responseData.put("totalElements", response.getTotalElements());
+        responseData.put("totalPages", response.getTotalPages());
+        responseData.put("size", response.getSize());
+        responseData.put("first", response.isFirst());
+        responseData.put("last", response.isLast());
+        responseData.put("numberOfElements", response.getNumberOfElements());
+
+        return ResponseEntity.ok(apiResponseUtil.success(responseData));
     }
 
     @GetMapping("/{courseId}")
