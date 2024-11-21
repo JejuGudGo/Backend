2632d58affc1258174200f3783f7180f0d152d2a
codesooo <zlsoo9ee@gmail.com>
2024-11-22 04:18:18 +0900

    Fix : 미반영 부분 수정 (#98)

--- src/main/java/com/example/jejugudgo/domain/course/jejugudgo/docs/JejuGudgoCourseDocument.java
+++ src/main/java/com/example/jejugudgo/domain/course/jejugudgo/docs/JejuGudgoCourseDocument.java
@@ -48,7 +48,6 @@ public class JejuGudgoCourseDocument {
 
     private double endLongitude;
 
-    private boolean isDeleted;
 
     @Field(type = FieldType.Nested, includeInParent = true)
     private List<JejuGudgoCourseSpotDocument> spots;
@@ -58,6 +57,7 @@ public class JejuGudgoCourseDocument {
 
     @Field(type = FieldType.Nested, includeInParent = true)
     private List<Long> bookmarkUsers;
+
 
     public static JejuGudgoCourseDocument of(JejuGudgoCourse jejuGudgoCourse, List<JejuGudgoCourseSpotDocument> spots, List<JejuGudgoCourseTagDocument> tags, List<Long> bookmarkUsers) {
         JejuGudgoCourseDocument document = new JejuGudgoCourseDocument();
@@ -81,7 +81,19 @@ public class JejuGudgoCourseDocument {
         document.setSpots(spots);
         document.setTags(tags);
         document.setBookmarkUsers(bookmarkUsers);
-        document.setDeleted(jejuGudgoCourse.isDeleted());
+
         return document;
+    }
+
+    public JejuGudgoCourseDocument updateStarAvg(double starAvg) {
+        JejuGudgoCourseDocument jejuGudgoCourseDocument = this;
+        jejuGudgoCourseDocument.setStarAvg(starAvg);
+        return jejuGudgoCourseDocument;
+    }
+
+    public JejuGudgoCourseDocument updateBookmarkUsers(List<Long> bookmarkUsers) {
+        JejuGudgoCourseDocument jejuGudgoCourseDocument = this;
+        jejuGudgoCourseDocument.setBookmarkUsers(bookmarkUsers);
+        return jejuGudgoCourseDocument;
     }
 }
