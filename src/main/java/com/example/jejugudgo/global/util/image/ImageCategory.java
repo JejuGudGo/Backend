66c49a16b4d5a376f1525829df2aa9668659609f
sieunnnn <wldsmtldsm65@gmail.com>
2024-12-06 18:13:15 +0900

    Fix: 유틸 점검 완료 (#102)

---
+++ src/main/java/com/example/jejugudgo/global/util/image/ImageCategory.java
@@ -0,0 +1,8 @@
+package com.example.jejugudgo.global.util.image;
+
+public enum ImageCategory {
+    PROFILE,
+    REVIEW,
+    COURSE,
+    WALKING,
+}
--- src/main/java/com/example/jejugudgo/global/util/image/ImageUtil.java
+++ src/main/java/com/example/jejugudgo/global/util/image/ImageUtil.java
@@ -1,9 +1,8 @@
-package com.example.jejugudgo.global.util;
+package com.example.jejugudgo.global.util.image;
 
 import com.example.jejugudgo.global.exception.enums.RetCode;
 import com.example.jejugudgo.global.exception.exception.CustomException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,17 +12,18 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class ImageUtil {
-    public Path setImagePath(String imageCategory) throws IOException {
+    public Path setImagePath(ImageCategory imageCategory) throws IOException {
         // 사용자 홈 디렉토리에 images 디렉토리 생성
         String userHome = System.getProperty("user.home");
-        Path rootLocation = Paths.get(userHome + "/images/" + imageCategory);
+        Path rootLocation = Paths.get(userHome + "/images/" + imageCategory.toString());
+
         // 해당 경로의 디렉토리가 없다면 생성
         Files.createDirectories(rootLocation);
 
         return rootLocation;
     }
 
-    public Path saveImage(Long userId, MultipartFile multipartFile, String imageCategory) throws Exception {
+    public Path save(Long userId, MultipartFile multipartFile, ImageCategory imageCategory) throws Exception {
         Path rootLocation = setImagePath(imageCategory);
 
         // 파일 이름, 확장자 추출
@@ -46,22 +46,11 @@ public class ImageUtil {
             return imageUrl;
 
         } catch (IOException e) {
-            throw new CustomException(RetCode.RET_CODE12);
+            throw new CustomException(RetCode.RET_CODE93);
         }
     }
 
-    public void deleteImage(Long userId, String imageCategory) throws IOException {
-        Path rootLocation = setImagePath(imageCategory);
-        DirectoryStream<Path> stream = Files.newDirectoryStream(rootLocation);
-
-        for (Path path : stream) {
-            if (path.getFileName().toString().startsWith(userId + "_")) {
-                Files.delete(path);
-            }
-        }
-    }
-
-    public void deleteImageWithUrl(String url) throws IOException {
+    public void delete(String url) throws IOException {
         Path imageLocation = Paths.get(url).normalize().toAbsolutePath();
 
         try {
@@ -69,10 +58,10 @@ public class ImageUtil {
 
         } catch (IOException e) {
             if (Files.notExists(imageLocation)) {
-                throw new CustomException(RetCode.RET_CODE13);
+                throw new CustomException(RetCode.RET_CODE94);
 
             } else {
-                throw new CustomException(RetCode.RET_CODE14);
+                throw new CustomException(RetCode.RET_CODE95);
             }
         }
     }
