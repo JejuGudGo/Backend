6c85e38d407100e428182fbded95455724e8dc38
codesooo <zlsoo9ee@gmail.com>
2024-12-11 23:07:48 +0900

    Feat : 걷기 옵션 enum 생성 (#103)

--- src/main/java/com/example/jejugudgo/domain/course/tamp/entity/WalkingPath.java
+++ src/main/java/com/example/jejugudgo/domain/course/tamp/entity/WalkingPath.java
@@ -1,4 +1,25 @@
 package com.example.jejugudgo.domain.course.tamp.entity;
 
+import com.example.jejugudgo.domain.mygudgo.course.entity.UserJejuGudgoCourse;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WalkingPath {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userJejuGudgoCourseId")
+    private UserJejuGudgoCourse userJejuGudgoCourse;
+
+
 }
