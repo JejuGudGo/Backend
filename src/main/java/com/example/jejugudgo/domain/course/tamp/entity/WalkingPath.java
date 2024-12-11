fe99636f20bddd1b53564de14e7d9913143f2d28
codesooo <zlsoo9ee@gmail.com>
2024-12-11 23:13:16 +0900

    Feat(WalkingPath) : 엔티티 생성 (#103)

--- src/main/java/com/example/jejugudgo/domain/course/tamp/entity/WalkingPath.java
+++ src/main/java/com/example/jejugudgo/domain/course/tamp/entity/WalkingPath.java
@@ -1,11 +1,14 @@
 package com.example.jejugudgo.domain.course.tamp.entity;
 
 import com.example.jejugudgo.domain.mygudgo.course.entity.UserJejuGudgoCourse;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Getter
@@ -21,5 +24,7 @@ public class WalkingPath {
     @JoinColumn(name = "userJejuGudgoCourseId")
     private UserJejuGudgoCourse userJejuGudgoCourse;
 
-
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private String lineData;
 }
