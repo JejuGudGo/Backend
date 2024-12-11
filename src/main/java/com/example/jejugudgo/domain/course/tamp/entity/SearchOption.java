de9df413fc3d53a3734f096111693653594e20f5
codesooo <zlsoo9ee@gmail.com>
2024-12-11 23:12:48 +0900

    Feat : 걷기 옵션 enum 생성 (#103)

--- src/main/java/com/example/jejugudgo/domain/course/tamp/entity/SearchOption.java
+++ src/main/java/com/example/jejugudgo/domain/course/tamp/entity/SearchOption.java
@@ -1,4 +1,29 @@
 package com.example.jejugudgo.domain.course.tamp.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum SearchOption {
+    SEARCH_OPTION01("0", "추천(기본값)"),
+    SEARCH_OPTION02("4", "추천+대로우선"),
+    SEARCH_OPTION03("10", "최단"),
+    SEARCH_OPTION("30", "최단거리+계단제외");
+
+    private final String searchOptionId;
+    private final String searchOptionName;
+
+    SearchOption(String searchOptionId, String searchOptionName) {
+        this.searchOptionId = searchOptionId;
+        this.searchOptionName = searchOptionName;
+    }
+
+    public static SearchOption fromSearchOptionId(String searchOptionId) {
+        for (SearchOption searchOption : SearchOption.values()) {
+            if (searchOption.searchOptionId.equals(searchOptionId)) {
+                return searchOption;
+            }
+        }
+        return null;
+    }
+
 }
