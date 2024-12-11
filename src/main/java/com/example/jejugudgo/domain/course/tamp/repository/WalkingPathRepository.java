3e38e423c79b4febd518e458c8827ff35a83c10c
codesooo <zlsoo9ee@gmail.com>
2024-12-11 23:24:25 +0900

    Feat : repository 생성 (#103)

---
+++ src/main/java/com/example/jejugudgo/domain/course/tamp/repository/WalkingPathRepository.java
@@ -0,0 +1,7 @@
+package com.example.jejugudgo.domain.course.tamp.repository;
+
+import com.example.jejugudgo.domain.course.tamp.entity.WalkingPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalkingPathRepository extends JpaRepository<WalkingPath, Long> {
+}
